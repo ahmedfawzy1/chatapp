ef1126c2b121e5b2c45aece61f65f09c96e3068f
ahmedfawzy1 <ahmedfawzy100038@gmail.com>
2025-03-29 16:01:11 +0200

    feat: add message routing to the API

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv/config.js";
 import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 
@@ -13,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
