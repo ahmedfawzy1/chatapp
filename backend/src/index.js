0a8628b7908605f04d3f0a89df796e28cf8402f4
ahmedfawzy1 <ahmedfawzy100038@gmail.com>
2025-03-29 15:25:09 +0200

    feat: add user authentication

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import dotenv from "dotenv/config.js";
+import cookieParser from "cookie-parser";
+
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
-app.listen(5001, () => {
-  console.log("Server running on port 5001");
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+  connectDB();
 });
